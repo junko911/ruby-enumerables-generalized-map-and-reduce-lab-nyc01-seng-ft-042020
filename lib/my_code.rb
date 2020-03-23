b878e9a4a8498d68f19f0dab34eb432fe156e5e5
Junko Tahara <junkotahara911@gmail.com>
2020-03-23 02:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
-# Your Code Here
+# # Your Code Here
+def map(array)
+  new_array = []
+  
+  index = 0
+  while index < array.length do
+    new_array.push yield(array[index])
+    index += 1
+  end
+  
+  new_array
+end
+
+
+def reduce(array, int = 0)
+  
+
+
+
+end
