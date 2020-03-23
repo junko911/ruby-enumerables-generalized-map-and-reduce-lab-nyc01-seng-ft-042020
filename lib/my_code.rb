b2c17bacfca5bf31bed2d04eb6a3ed864a6d8694
Junko Tahara <junkotahara911@gmail.com>
2020-03-23 03:37:49 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,11 +16,12 @@ def reduce(array, int = nil)
   
   if int
     total = int
+    index = 0
   else
     total = array[0]
+    index = 1
   end
   
-  index = 0
   while index < array.length do
     total = yield(total, array[index])
     index += 1
